f8e3b2ce6776bddcf1669cf5688d5a0087281c3f
ntibi <ntibi@protonmail.com>
2025-04-02 20:18:07 +0200

    wip ik solver

--- src/ik.rs
+++ src/ik.rs
@@ -3,7 +3,9 @@ use bevy::prelude::*;
 pub struct IKPlugin;
 
 impl Plugin for IKPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, apply_ik);
+    }
 }
 
 #[derive(Component)]
@@ -28,5 +30,22 @@ impl IKConstraint {
 
     pub fn untarget(&mut self) {
         self.target = None;
+    }
+}
+
+fn apply_ik(ik_constraints: Query<&IKConstraint>, mut transforms: Query<&mut Transform>) {
+    for constraint in ik_constraints.iter() {
+        if let Some(target) = constraint.target {
+            let mut chain = constraint
+                .chain
+                .iter()
+                .map(|entity| (*entity, transforms.get(*entity).unwrap().translation.xy()))
+                .collect::<Vec<_>>();
+
+            for (entity, new_pos) in chain {
+                let mut transform = transforms.get_mut(entity).unwrap();
+                transform.translation = new_pos.extend(transform.translation.z);
+            }
+        }
     }
 }
