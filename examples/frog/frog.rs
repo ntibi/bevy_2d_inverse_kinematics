509f5f9a0e96157c5795abdf491864bd0ec421c4
ntibi <ntibi@protonmail.com>
2025-04-09 16:48:58 +0200

    cleanup frog example

--- examples/frog/frog.rs
+++ examples/frog/frog.rs
@@ -1,13 +1,27 @@
-use bevy::prelude::*;
-use bevy_2d_inverse_kinematics::{Bone, IKConstraint, JointConstraint};
+use bevy::{prelude::*, window::PrimaryWindow};
+use bevy_2d_inverse_kinematics::{IKConstraint, JointConstraint};
 use std::f32::consts::PI;
 
 pub struct FrogPlugin;
 
 impl Plugin for FrogPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup)
-            .add_systems(Update, (input, move_animal, compute_foot_placement).chain());
+        app.add_systems(Startup, setup).add_systems(
+            Update,
+            (
+                input,
+                move_animal,
+                (
+                    compute_foot_placement.run_if(|mouse: Res<ButtonInput<MouseButton>>| {
+                        !mouse.pressed(MouseButton::Left)
+                    }),
+                    update_target.run_if(|mouse: Res<ButtonInput<MouseButton>>| {
+                        mouse.pressed(MouseButton::Left)
+                    }),
+                ),
+            )
+                .chain(),
+        );
     }
 }
 
@@ -26,7 +40,7 @@ fn spawn_arm(
     let mut entities = Vec::new();
 
     let get_limb_world_pos =
-        |i: usize| (pos + dir.normalize() * i as f32 * 6.).extend(-2. - i as f32);
+        |i: usize| (pos + dir.normalize() * i as f32 * dist_constraint).extend(2. - i as f32);
 
     for i in 0..len {
         let id = commands
@@ -95,6 +109,11 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    println!("press WASD to move");
+    println!("press Q and E to rotate");
+    println!("press SPACE to show foot placement debug");
+    println!("press LEFT MOUSE BUTTON to manually set IK target");
+
     let color = Color::srgb(0.0, 0.8, 0.0);
     let hand_color = Color::srgb(0.0, 0.0, 0.0);
 
@@ -124,13 +143,13 @@ fn setup(
         })
         .id();
 
-    let parts = 5;
+    let parts = 4;
     let dist_constraint = 6.;
-    let max_distance = dist_constraint * (parts - 1) as f32 * 0.9;
+    let max_distance = dist_constraint * (parts - 1) as f32 * 0.95;
 
     // bottom right leg
-    let pos = Vec2::new(12., -17.);
-    let next_step = Vec2::new(14., 2.);
+    let pos = Vec2::new(16., -17.);
+    let next_step = Vec2::new(8., 2.);
     let (anchor, effector) = spawn_arm(
         pos,
         Vec2::new(1., 0.),
@@ -153,8 +172,8 @@ fn setup(
     ));
 
     // bottom left leg
-    let pos = Vec2::new(-12., -17.);
-    let next_step = Vec2::new(-14., 2.);
+    let pos = Vec2::new(-16., -17.);
+    let next_step = Vec2::new(-8., 2.);
     let (anchor, effector) = spawn_arm(
         pos,
         Vec2::new(-1., 0.),
@@ -177,8 +196,8 @@ fn setup(
     ));
 
     // top right leg
-    let pos = Vec2::new(12., 17.);
-    let next_step = Vec2::new(15., 3.);
+    let pos = Vec2::new(16., 17.);
+    let next_step = Vec2::new(9., 3.);
     let (anchor, effector) = spawn_arm(
         pos,
         Vec2::new(1., 0.),
@@ -201,8 +220,8 @@ fn setup(
     ));
 
     // top left leg
-    let pos = Vec2::new(-12., 17.);
-    let next_step = Vec2::new(-15., 3.);
+    let pos = Vec2::new(-16., 17.);
+    let next_step = Vec2::new(-9., 3.);
     let (anchor, effector) = spawn_arm(
         pos,
         Vec2::new(-1., 0.),
@@ -274,13 +293,14 @@ fn move_animal(mut query: Query<(&mut Transform, &Velocity, &AngularVelocity)>, 
 }
 
 fn compute_foot_placement(
+    // TODO use agent velocity dir to offset foot zone
     _agent: Query<(&Transform, &Velocity), With<AnimalThingy>>,
     mut foot_zones: Query<(&GlobalTransform, &FootZone, &Parent)>,
     mut effectors: Query<(&mut IKConstraint, &GlobalTransform)>,
+    mut gizmos: Gizmos,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
 ) {
     for (transform, foot_zone, _parent) in foot_zones.iter_mut() {
-        //let (agent_transform, agent_vel) = agent.get(**parent).unwrap();
-
         let (mut effector, foot_pos) = effectors.get_mut(foot_zone.foot_entity).unwrap();
         let foot_pos = foot_pos.translation().xy();
         let base_pos = transform.translation().xy();
@@ -290,18 +310,37 @@ fn compute_foot_placement(
                 .mul_vec3(foot_zone.default_rest_pos.extend(0.))
                 .xy();
 
-        //gizmos.circle_2d(default_rest_pos, 3., Color::srgb(1., 0., 0.));
-
-        //let movement_dir = agent_transform
-        //.rotation
-        //.mul_vec3(agent_vel.extend(0.))
-        //.xy()
-        //.normalize_or_zero();
-
-        //gizmos.circle_2d(foot_pos, 1., Color::srgb(0., 1., 1.));
-        //gizmos.circle_2d(base_pos, foot_zone.max_distance, Color::srgb(0., 1., 0.));
         if foot_pos.distance(base_pos) > foot_zone.max_distance {
             effector.target(default_rest_pos);
+        }
+
+        if keyboard_input.pressed(KeyCode::Space) {
+            gizmos.circle_2d(default_rest_pos, 3., Color::srgb(1., 0., 0.));
+            gizmos.circle_2d(foot_pos, 1., Color::srgb(0., 1., 1.));
+            gizmos.circle_2d(base_pos, foot_zone.max_distance, Color::srgb(0., 1., 0.));
+        }
+    }
+}
+
+fn update_target(
+    window: Query<&Window, With<PrimaryWindow>>,
+    camera: Query<(&Camera, &GlobalTransform)>,
+    mut query: Query<&mut IKConstraint>,
+    buttons: Res<ButtonInput<MouseButton>>,
+) {
+    if !buttons.pressed(MouseButton::Left) {
+        return;
+    }
+
+    let (camera, camera_transform) = camera.single();
+    let window = window.single();
+
+    if let Some(pos) = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world_2d(camera_transform, cursor).ok())
+    {
+        for mut ik in query.iter_mut() {
+            ik.target(pos);
         }
     }
 }
